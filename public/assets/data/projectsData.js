e06d6657e0edca26ed346f465ff465bc0510b3ca
Marquesjob <patrickmarques.job@hotmail.com>
2022-06-06 17:12:00 -0300

    modificacoes na estrutura de arquivos

---
+++ public/assets/data/projectsData.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        link: 'link',
+        repIcon: 'repIcon',
+        htmlIcon: 'htmlIcon',
+        jsIcon: 'jsIcon',
+        cssIcon: 'cssIcon',
+        htmlIcon: 'htmlIcon',
+    }
+]
