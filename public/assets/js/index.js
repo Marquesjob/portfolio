3eaa09303ffa7144d960b3394911e52bcd64fd93
Marquesjob <patrickmarques.job@hotmail.com>
2022-06-13 19:07:46 -0300

    - ""

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -15,21 +15,18 @@ for (let i = 0; i < projectsData.length; i++) {
       <div class="card-body">
         <p class="card-text __font">${projectsData[i].description}</p>
         <div class="input-group input-group-sm mb-3">
-          <button class="btn border-0 border-opacity-25 d-flex align-items-center" type="button"><embed
-            class="d-inline mx-2 svg-sizing icon-effects" src="./assets/svg/link-solid.svg" /></button>
-          <input type="text" class="form-control fs-6 text-muted" aria-label="Example text with button addon"
-            aria-describedby="button-addon1" value="${projectsData[i].linkView}" disabled>
+          <a id="projects__button__linkIcon" class="btn border-0 border-opacity-25 d-flex align-items-center" type="button">
+            <img  id="projects__linkIcon" class="d-inline mx-2 svg-sizing icon-effects" src="./assets/svg/link-solid.svg" />
+          </a>
+          <input type="text" class="form-control fs-6 text-muted" value="${projectsData[i].linkView}" disabled>
         </div>
         <div class="input-group input-group-sm mb-3">
-          <button class="btn border-0 border-opacity-25  d-flex align-items-center" type="button"><embed
-            class="d-inline mx-2 svg-sizing icon-effects" src="./assets/svg/github-grey.svg" /></button>
+          <a id="projects__button__gitIcon" class="btn border-0 border-opacity-25  d-flex align-items-center">
+            <img id="projects__gitIcon" class="d-inline mx-2 svg-sizing" src="./assets/svg/github-grey.svg"/>
+          </a>
           <input type="text" class="form-control fs-6 text-muted" value="${projectsData[i].linkRepo}" disabled>
         </div>
       </div>
     </div>`;
   projects__dinamic__row.appendChild(element);
 }
-
-
-
-/**/
