c6f70bc4e4b2a90be9c7a53ef5368725fbd21cc5
Marquesjob <patrickmarques.job@hotmail.com>
2022-07-01 20:04:52 -0300

    - instalando vue

---
+++ public-vue/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Wave from '@/components/icons/wave.vue';
+import Profile from '@/components/Profile.vue';
+</script>
+
+<template>
+  <main>
+    <Wave />
+    <Profile />
+
+    
+  </main>
+</template>
