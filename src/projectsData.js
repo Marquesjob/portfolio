fa17d780792c31dbe90f00fc8f11d2965b59f2e4
Marquesjob <patrickmarques.job@hotmail.com>
2022-07-07 02:48:49 -0300

    - atualizando lista de projetos

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,29 +1,44 @@
 const projects = [
+    {   
+        title:'Portfólio - 2022',
+        linkRepo: 'https://github.com/Marquesjob/portfolio',
+        linkView: 'https://patrick-b-marques.web.app/',
+        description: 'Inicialmente feito com Javascript Vanilla, HTML e CSS puros. Atualmente utilizando VueJS e Bootstrap para a evolução do template.',
+    },
+    {   
+        title:'Whatsapp Web - 2022',
+        linkRepo: 'https://github.com/Marquesjob/interface-wpp-vuejs',
+        linkView: 'https://interface-wpp-vuejs.web.app/',
+        description: 'Clone da interface do Whatsapp Web com troca de mensagens. Feito com Vuejs e Bulma(CSS), explorando recursos como; diretivas, props e variaveis por meio da reatividade.',
+    },
 
     {   
-        title:'Music Player - 2022',
-        logo: 'https://picsum.photos/295/250',
+        title:'Comment Box - 2022',
+        linkRepo: 'https://github.com/Marquesjob/interface-wpp-vuejs',
+        linkView: 'https://interface-wpp-vuejs.web.app/',
+        description: 'Caixa de comentários no estilo todo-list feita com Vuejs e Bootstrap, explorando recursos como; Event Handing, props e diretivas.',
+    },
+
+    {   
+        title:'Music Player - 2022',     
         linkRepo: 'https://github.com/Marquesjob/music-player-vanilla-js',
         linkView: 'https://music-player-vanilla-js.firebaseapp.com/',
         description: 'Music player criado utilizando Javascript Vanilla, HTML e CSS (Materialize).',
     },
     {   
-        title:'Todo-list - 2021',
-        logo: 'https://picsum.photos/295/250',
+        title:'Todo-list - 2021',   
         linkRepo: 'https://github.com/Marquesjob/todo-list-js',
         linkView: 'https://tazkz-app.web.app/',
         description: 'Todo-list desenvolvido utilizando Javascript, Bootstrap, aplicando conhecimentos em manipulação do DOM e Local Storage',
     },
     {   
-        title:'Site de Receitas - 2020',
-        logo: 'https://picsum.photos/295/250',
+        title:'Site de Receitas - 2020',    
         linkRepo: 'https://github.com/Marquesjob/curso-alura-arquitetura-css',
         linkView: 'https://hortifruti-site.web.app/',
         description: 'Projeto da Alura onde foi montado um site de receitas, passando conceitos como a metodologia BEM e uma boa organização de arquivos CSS.',
     },
     {   
         title:'Barbearia - 2020',
-        logo: 'https://picsum.photos/295/250',
         linkRepo: 'https://github.com/Marquesjob/curso-alura-html',
         linkView: 'https://barbearia-curso-alura.web.app/',
         description: 'Projeto de um site de barbearia feito inteiramente com HTML5 e CSS no curso Alura Front-End',
