bade3208ae67adb849802f1e89c056cf7598cf21
Marquesjob <patrickmarques.job@hotmail.com>
2022-07-14 19:08:18 -0300

    - inserindo novo projeto

---
+++ src/projectsData.js
@@ -0,0 +1,62 @@
+const projects = [
+    {   
+        title:'GetHub - 2022',
+        linkRepo: 'https://ogethub.web.app/',
+        linkView: 'https://patrick-b-marques.web.app/',
+        description: 'Projeto pessoal que busca repositórios git de um perfil especificado. Interface desenvolvida com Vuejs e CSS. Requisições feitas atráveis do cliente HTTP Axios.',
+    },
+    {   
+        title:'Portfólio - 2022',
+        linkRepo: 'https://github.com/Marquesjob/portfolio',
+        linkView: 'https://patrick-b-marques.web.app/',
+        description: 'Inicialmente feito com Javascript Vanilla, HTML e CSS puros. Atualmente utilizando VueJS e Bootstrap para a evolução do template.',
+    },
+    {   
+        title:'Whatsapp Web - 2022',
+        linkRepo: 'https://github.com/Marquesjob/interface-wpp-vuejs',
+        linkView: 'https://interface-wpp-vuejs.web.app/',
+        description: 'Clone da interface do Whatsapp Web que simula troca de mensagens. Feito com Vuejs e Bulma(CSS), explorando recursos como; diretivas, props e variaveis por meio da reatividade.',
+    },
+
+    {   
+        title:'Comment Box - 2022',
+        linkRepo: 'https://github.com/Marquesjob/coment-box-vuejs',
+        linkView: 'https://coment-box-vuejs.web.app/',
+        description: 'Caixa de comentários no estilo todo-list feita com Vuejs e Bootstrap, explorando recursos como; Event Handing, props e diretivas.',
+    },
+
+    {   
+        title:'Music Player - 2022',     
+        linkRepo: 'https://github.com/Marquesjob/music-player-vanilla-js',
+        linkView: 'https://music-player-vanilla-js.firebaseapp.com/',
+        description: 'Music player criado utilizando Javascript Vanilla, HTML e CSS (Materialize).',
+    },
+    {   
+        title:'Todo-list - 2021',   
+        linkRepo: 'https://github.com/Marquesjob/todo-list-js',
+        linkView: 'https://tazkz-app.web.app/',
+        description: 'Todo-list desenvolvido utilizando Javascript, Bootstrap, aplicando conhecimentos em manipulação do DOM e Local Storage',
+    },
+    {   
+        title:'Site de Receitas - 2020',    
+        linkRepo: 'https://github.com/Marquesjob/curso-alura-arquitetura-css',
+        linkView: 'https://hortifruti-site.web.app/',
+        description: 'Projeto da Alura onde foi montado um site de receitas, passando conceitos como a metodologia BEM e uma boa organização de arquivos CSS.',
+    },
+    {   
+        title:'Apperia - 2020',
+        logo: 'https://picsum.photos/295/250',
+        linkRepo: 'https://github.com/Marquesjob/curso-alura-layouts-responsivos',
+        linkView: 'https://site-apperia.web.app/',
+        description: 'Site Apperia, feito com HTML CSS, aplicando conceitos de responsividade e metodologia BEM (Layout pensado e projetado para dispositivos Mobile).',
+    },
+    {   
+        title:'Barbearia - 2020',
+        linkRepo: 'https://github.com/Marquesjob/curso-alura-html',
+        linkView: 'https://barbearia-curso-alura.web.app/',
+        description: 'Projeto de um site de barbearia feito inteiramente com HTML5 e CSS no curso Alura Front-End',
+    },
+
+]
+
+export default projects;
